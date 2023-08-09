059a1e89d68d7c21fe404fd15f6d4abf7edb74bc
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-08-09 14:24:17 +0900

    feat : dot product

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -8,6 +8,12 @@ void main() {
 	vec2 uv = vUv;
 	uv -= vec2(0.5);
 	uv *= 2.0;
-	
-	gl_FragColor = vec4( vec3(step(uRadius, length(uv))), 1.0 );
+
+	vec3 vectorA = vec3(1.0);
+	vec3 vectorB = vec3(0.0);
+
+	float dotProduct = dot(vectorA, vectorB);
+	// vectorA.x * vectorB.x + vectorA.y * vectorB.y + vectorA.z * vectorB.z
+
+	gl_FragColor = vec4(vec3(dotProduct), 1.0);
 }
