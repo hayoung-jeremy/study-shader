2f5be473351ed9bc4ed9f6744abd47463edac967
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-08-16 14:25:28 +0900

    feat : desaturation effect

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -7,7 +7,10 @@ varying vec2 vUv;
 
 void main() {
 
-	vec4 color = texture2D(uTexture, vUv);
+	const vec3 DESATURATE = vec3(0.2126, 0.7152, 0.0722);
+	vec3 color = texture2D(uTexture, vUv).xyz;
 
-	gl_FragColor = vec4(color.xyz, 1.0);
+	float finalColor = dot(DESATURATE, color);
+
+	gl_FragColor = vec4(vec3(finalColor), 1.0);
 }
