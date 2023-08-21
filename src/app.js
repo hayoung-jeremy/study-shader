00b6f26f13d502988eae4b42ce2a01808e25501f
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2023-08-21 10:27:47 +0900

    feat : update shader
    issues :
    * shader string을 직접 수정하여 추가하는 과정에서 shader compile 불가, 대안 연구 필요

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import * as THREE from 'three'
 import { addPass, useCamera, useGui, useRenderSize, useScene, useTick } from './render/init.js'
 // import postprocessing passes
@@ -6,11 +5,13 @@ import { SavePass } from 'three/examples/jsm/postprocessing/SavePass.js'
 import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
 import { BlendShader } from 'three/examples/jsm/shaders/BlendShader.js'
 import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
 
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 
-import colorfulTexture from './images/image.jpg'
+import vertexPars from './shaders/vertex_pars.glsl'
+import vertexMain from './shaders/vertex_main.glsl'
 
 const startApp = () => {
   const scene = useScene()
@@ -22,23 +23,36 @@ const startApp = () => {
   const MOTION_BLUR_AMOUNT = 0.725
 
   // lighting
-  const dirLight = new THREE.DirectionalLight('#ffffff', 0.75)
-  dirLight.position.set(5, 5, 5)
+  const dirLight = new THREE.DirectionalLight('#526cff', 0.6)
+  dirLight.position.set(2, 2, 2)
 
-  const ambientLight = new THREE.AmbientLight('#ffffff', 0.2)
+  const ambientLight = new THREE.AmbientLight('#4255ff', 0.5)
   scene.add(dirLight, ambientLight)
 
   // meshes
-  const geometry = new THREE.PlaneGeometry(2, 2)
-  const material = new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    // wireframe: true,
+  const geometry = new THREE.IcosahedronGeometry(1, 400)
+  const material = new THREE.MeshStandardMaterial({
+    onBeforeCompile: (shader) => {
+      // storing a reference to the shader object
+      material.userData.shader = shader
+
+      // uniforms
+      shader.uniforms.uTime = { value: 0 }
+
+      const parsVertexString = /* glsl */ '#include <displacementmap_pars_vertex>\n'
+      shader.vertexShader = shader.vertexShader.replace(
+        parsVertexString,
+        parsVertexString + vertexPars
+      )
+
+      const mainVertexString = /* glsl */ `#include <displacementmap_vertex>\n`
+      shader.vertexShader = shader.vertexShader.replace(
+        mainVertexString,
+        mainVertexString + vertexMain
+      )
+      console.log(shader.vertexShader)
+    },
   })
-  material.uniforms.uTime = { value: 0 }
-  material.uniforms.uRadius = { value: 0.5 }
-  material.uniforms.uTexture = { value: new THREE.TextureLoader().load(colorfulTexture) }
-  console.log(material)
 
   const ico = new THREE.Mesh(geometry, material)
   scene.add(ico)
@@ -48,8 +62,6 @@ const startApp = () => {
   cameraFolder.add(camera.position, 'z', 0, 10)
   cameraFolder.open()
 
-  gui.add(material.uniforms.uRadius, 'value').min(0).max(1)
-
   // postprocessing
   const renderTargetParameters = {
     minFilter: THREE.LinearFilter,
@@ -57,24 +69,13 @@ const startApp = () => {
     stencilBuffer: false,
   }
 
-  // save pass
-  const savePass = new SavePass(new THREE.WebGLRenderTarget(width, height, renderTargetParameters))
+  // postprocessing
+  addPass(new UnrealBloomPass(new THREE.Vector2(width, height), 0.7, 0.4, 0.4))
 
-  // blend pass
-  const blendPass = new ShaderPass(BlendShader, 'tDiffuse1')
-  blendPass.uniforms['tDiffuse2'].value = savePass.renderTarget.texture
-  blendPass.uniforms['mixRatio'].value = MOTION_BLUR_AMOUNT
-
-  // output pass
-  const outputPass = new ShaderPass(CopyShader)
-  outputPass.renderToScreen = true
-
-  // adding passes to composer
-  addPass(blendPass)
-  addPass(savePass)
-  addPass(outputPass)
-
-  useTick(({ timestamp, timeDiff }) => {})
+  useTick(({ timestamp, timeDiff }) => {
+    const time = timestamp / 5000
+    material.userData.shader.uniforms.uTime.value = time
+  })
 }
 
 export default startApp
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,16 +1,10 @@
-uniform sampler2D uTexture;
-uniform float uRadius;
+uniform float uTime;
 
 varying vec3 vPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
+varying float vDisplacement;
 
 void main() {
-
-	const vec3 DESATURATE = vec3(0.2126, 0.7152, 0.0722);
-	vec3 color = texture2D(uTexture, vUv).xyz;
-
-	float finalColor = dot(DESATURATE, color);
-
-	gl_FragColor = vec4(vec3(finalColor), 1.0);
+	gl_FragColor = vec4(vec3(0.1, 0.3, 0.8), 1.0);
 }
